d3c5df02dd87473c4ec1fede9e5dc4105e4a7742
mauriciosd <mauriciosd@hotmail.com>
2022-08-12 17:14:09 -0300

    test

--- pages/[astroName]/index.jsx
+++ pages/[astroName]/index.jsx
@@ -4,21 +4,25 @@ const AstroDetails = (props) => {
     return <AstroSection data={props.astroData}/>
 }
 
-export const getStaticPaths = async () => {
-    const data = await fetch(`${process.env.DOMAIN}api/en/getPaths`, {
-        method: "GET",
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }).then(async (res) => res.json())
+// export const getStaticPaths = async () => {
+//     const data = await fetch(`${process.env.DOMAIN}api/en/getPaths`, {
+//         method: "GET",
+//         headers: {
+//             'Content-Type': 'application/json'
+//         }
+//     }).then(async (res) => res.json()).then(x => {
+//         console.log(x)
+//         return x
+//     })
 
-    return {
-        fallback: false,
-        paths: data.map((astro) => (
-            { params: { astroName: astro.en_name } }
-        ))
-    }
-}
+
+//     return {
+//         fallback: false,
+//         paths: data.map((astro) => (
+//             { params: { astroName: astro.en_name } }
+//         ))
+//     }
+// }
 
 export const getStaticProps = async (ctx) => {
     const data = await fetch(`${process.env.DOMAIN}api/en/getInfos/${ctx.params.astroName}`, {
